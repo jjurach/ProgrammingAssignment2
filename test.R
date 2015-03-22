653abf69dd7b0cba1e7d338dbb2747097776865d
James Jurach <james.jurach@gmail.com>
2015-03-22 08:00:56 -0500

    tests for cachematrix.R

---
+++ test.R
@@ -0,0 +1,24 @@
+
+source("cachematrix.R")
+
+dumpMatrix <- function(x, name="") {
+  print(name)
+  print(x)
+}
+
+#matrix <- matrix(1:4, nrow=2)
+#matrix <- matrix(-3:5, nrow=3)
+matrix <- matrix(rexp(9, rate=1), ncol=3)
+
+cacheMatrix <- makeCacheMatrix(matrix)
+#cacheMatrix$set(matrix)
+
+solved <- cacheSolve(cacheMatrix)
+solved <- cacheSolve(cacheMatrix)
+solved <- cacheSolve(cacheMatrix)
+solved2 <- solve(matrix)
+
+dumpMatrix(matrix, 'orig')
+dumpMatrix(solved, 'solved')
+dumpMatrix(solved - solved2, 'should zero')
+
